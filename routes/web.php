3ef42ed7d549b09b71e89a5791d3e116b6144de7
Mehmet Deniz Kumcu <68655812+devmdeniz@users.noreply.github.com>
2023-05-21 20:15:48 +0300

    Laravel Learning Day #2

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -8,38 +9,23 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
-/*
-! Similar With:
-Route::view('/','welcome');
-*/
-Route::view('/a','notwelcome');
 
-// Route::get("/a", function () {
-//     return view('notwelcome');
-// });
-Route::get("/profil", function () {return view('welcome');});
-Route::view("/kullanici", 'kullanici');
-// Route::redirect('/kullanici', '/profil');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// Route::prefix('profil')->group(function () {
-//         Route::view('/a','notwelcome');
-//         Route::get('/b', function() {
-//             return view('welcome');
-//         });
-// });
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-// Route::fallback(function() {
-//     Route::view("/hata", function() {
-//         return "Aradigin sayfa yok";
-//     });
-// }); -> // ! calismasi icin bir prefix sayfasinin icinde olmasi gerekiyor 
-
-    Route::view('/z','notwelcome')->name("zpage");
+require __DIR__.'/auth.php';
